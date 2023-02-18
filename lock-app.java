567358ddaf18e3799231681688a0b6ce7db55d7c
Jackisa Daniel Barack <87069100+bjackisa@users.noreply.github.com>
2023-02-18 16:54:30 +0300

    Create lock-app.java

---
+++ lock-app.java
@@ -0,0 +1,110 @@
+ // Java code 
+public class UnlockAndroid {
+	public static void main(String[] args) {
+		// Get the current pin
+		int currentPin = get a program to unlock an android 
+
+// Java code 
+public class UnlockAndroid {
+	public static void main(String[] args) {
+		// Get the current pin
+		int currentPin = getCurrentPin();
+		
+		// Generate a random pin
+		int newPin = generateRandomPin();
+		
+		// Set the new pin
+		setNewPin(newPin);
+		
+		// Unlock the device
+		unlockDevice(newPin);
+		
+		// Store the new pin in the device
+		storePin(newPin);
+		
+		// Delete the old pin
+		deleteOldPin(currentPin);
+		
+		// Lock the device
+		lockDevice();
+	}
+	
+	public static int getCurrentPin() {
+		// Code to get the current pin
+		return 0;
+	}
+	
+	public static int generateRandomPin() {
+		// Code to generate a random pin
+		return 0;
+	}
+	
+	public static void setNewPin(int newPin) {
+		// Code to set the new pin
+	}
+	
+	public static void unlockDevice(int newPin) {
+		// Code to unlock the device
+	}
+	
+	public static void storePin(int newPin) {
+		// Code to store the new pin in the device
+	}
+	
+	public static void deleteOldPin(int currentPin) {
+		// Code to delete the old pin
+	}
+	
+	public static void lockDevice() {
+		// Code to lock the device
+	}
+}		
+		// Generate a random pin
+		int newPin = generateRandomPin();
+		
+		// Set the new pin
+		setNewPin(newPin);
+		
+		// Unlock the device
+		unlockDevice(newPin);
+		
+		// Store the new pin in the device
+		storePin(newPin);
+		
+		// Delete the old pin
+		deleteOldPin(currentPin);
+		
+		// Lock the device
+		lockDevice();
+	}
+	
+	public static int getCurrentPin() {
+		// Code to get the current pin
+		return 0;
+	}
+	
+	public static int generateRandomPin() {
+		// Code to generate a random pin
+		return 0;
+	}
+	
+	public static void setNewPin(int newPin) {
+		// Code to set the new pin
+	}
+	
+	public static void unlockDevice(int newPin) {
+		// Code to unlock the device
+	}
+	
+	public static void storePin(int newPin) {
+		// Code to store the new pin in the device
+	}
+	
+	public static void deleteOldPin(int currentPin) {
+		// Code to delete the old pin
+	}
+	
+	public static void lockDevice() {
+		// Code to lock the device
+	}
+}
